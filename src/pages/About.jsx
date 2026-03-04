c96f929398a20d6a29e9e4f290ad58d71aa3ba2e
DESKTOP-PTDFKEA\bnfa <em692563@gmail.com>
2026-03-04 22:51:50 +0100

    Nuevo boton

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,13 @@
 function About() {
+  function handleClick() {
+    alert('¡Botón pulsado!');
+  }
+
   return (
     <div>
       <h1>Acerca de</h1>
-      <p>Esta es la página About con Cambio.</p>
+      <p>Esta es la página About.</p>
+      <button onClick={handleClick}>Púlsame</button>
     </div>
   );
 }
